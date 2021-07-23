2b4b6b71e149c0904ab6fa0464d40dfd80bd8e17
Bharat Poptwani <bharat.poptwani@gmail.com>
2021-07-23 21:08:04 +0530

    added a prop as an initial value to useState

---
+++ src/exercise/01.extra-1.js
@@ -0,0 +1,29 @@
+// useState: greeting
+// http://localhost:3000/isolated/exercise/01.js
+
+import React from 'react'
+
+function Greeting({initialName}) {
+  // 💣 delete this variable declaration and replace it with a React.useState call
+  let [name, setName] = React.useState(initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting initialName="Bharat" />
+}
+
+export default App
