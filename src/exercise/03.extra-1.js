0e60195fd71fa01e915dacf737c99bdb5fb526ad
Bharat Poptwani <bharat.poptwani@gmail.com>
2021-07-25 21:23:32 +0530

    enjoyed playing tic tac toe

---
+++ src/exercise/03.extra-1.js
@@ -0,0 +1,47 @@
+// Lifting state
+// http://localhost:3000/isolated/exercise/03.js
+
+import React from 'react'
+
+function Name() {
+  const [name, setName] = React.useState('')
+
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input id="name" value={name} onChange={event => setName(event.target.value)} />
+    </div>
+  )
+}
+
+// 🐨 accept `animal` and `onAnimalChange` props to this component
+function FavoriteAnimal({animal, onAnimalChange}) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input id="animal" value={animal} onChange={onAnimalChange} />
+    </div>
+  )
+}
+
+// 🐨 uncomment this
+function Display({name, animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
+}
+
+function App() {
+  const [animal, setAnimal] = React.useState('')
+
+  return (
+    <form>
+      <Name />
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={event => setAnimal(event.target.value)}
+      />
+      <Display animal={animal} />
+    </form>
+  )
+}
+
+export default App
