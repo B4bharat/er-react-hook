bf2f9549d0d6eae495a8ec6e80969f22bd51947c
Bharat Poptwani <bharat.poptwani@gmail.com>
2021-07-28 09:55:22 +0530

    added error state as well

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -16,6 +16,7 @@ import {
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
   const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
 
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
@@ -28,11 +29,25 @@ function PokemonInfo({pokemonName}) {
       return
     }
 
+    setError(null)
     setPokemon(null)
-    fetchPokemon(pokemonName).then(pokemonData => {
-      setPokemon(pokemonData)
-    })
+    fetchPokemon(pokemonName)
+      .then(pokemonData => {
+        setPokemon(pokemonData)
+      })
+      .catch(error => {
+        setError(error)
+      })
   }, [pokemonName])
+
+  if (error) {
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  }
 
   if (!pokemonName) {
     return 'Submit a pokemon'
